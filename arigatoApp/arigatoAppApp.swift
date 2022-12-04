e3fb331a9514ddd292154f60e30057b84c0c46c8
tomsan96 <gradada0507@gmail.com>
2022-12-04 19:08:46 +0900

    first commit

---
+++ arigatoApp/arigatoAppApp.swift
@@ -0,0 +1,16 @@
+//
+//  arigatoAppApp.swift
+//  arigatoApp
+//
+//
+
+import SwiftUI
+
+@main
+struct arigatoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
